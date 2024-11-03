7b1766fa16be6cf3eeae2d3dbd683210095af282
Corey Gray <16312666+csgray@users.noreply.github.com>
2024-11-02 21:33:33 -0400

    feat: Add pseudocode

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,18 @@
-console.log("Hello, World!")
+// Get the computer's choice from one of: rock, paper, scissors
+
+// Get the human's choice from one of: rock, paper, scissors
+
+// Determine who won the round:
+// - Rock beats scissors
+// - Scissors beats paper
+// - Paper beats rock
+
+// Increment the winner's (computer or human) score
+
+// Start another round
+
+// Play five rounds
+
+// Determine whoever won the most out of five rounds
+
+// Announce the winner
