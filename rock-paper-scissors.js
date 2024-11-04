103c709535054d0f30a3bc07169e5822062db407
Corey Gray <16312666+csgray@users.noreply.github.com>
2024-11-04 16:43:36 -0500

    feat: Add playRound function
    Takes humanChoice and computerChoice as parameters and determines who
    wins according to the rules for rock-paper-scissors. It logs a message
    explaining who won then returns the winner.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,7 @@
 // Get the computer's choice from one of: rock, paper, scissors
 
 /**
+ * Randomly returns "rock", "paper", or "scissors".
  * getComputerchoice takes no arguments.
  * @returns one of "rock", "paper", or "scissors".
  */
@@ -21,6 +22,7 @@ function getComputerChoice() {
 // Get the human's choice from one of: rock, paper, scissors
 
 /**
+ * Prompts the user for "rock", "paper", or "scissors" and returns it.
  * getHumanChoice takes no arguments.
  * @returns one of "rock", "paper", or "scissors".
  */
@@ -40,6 +42,60 @@ function getHumanChoice() {
 // - Rock beats scissors
 // - Scissors beats paper
 // - Paper beats rock
+
+/**
+ * Determines who won a round of rock-paper-scissors.
+ * It outputs a message explaining the outcome to the user and returns who won.
+ * @param {*} humanChoice must be one of "rock", "paper", or "scissors"
+ * @param {*} computerChoice must be one of "rock", "paper", or "scissors"
+ * @returns one of "human", "computer", or "tie"
+ */
+function playRound(humanChoice, computerChoice) {
+    console.log(`The computer chose: ${computerChoice}`)
+
+    if (humanChoice === "rock") {
+        if (computerChoice === "rock") {
+            console.log("Tie! Both you and the computer chose rock.")
+            return "tie"
+        } else if (computerChoice === "paper") {
+            console.log("You lose! Paper covers rock.")
+            return "computer"
+        } else if (computerChoice === "scissors") {
+            console.log("You win! Rock crushes scissors.")
+            return "human"
+        } else {
+            throw new SyntaxError(`${computerChoice} is an invalid computerChoice!`)
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You win! Paper covers rock.")
+            return "human"
+        } else if (computerChoice === "paper") {
+            console.log("Tie! Both you and the computer chose paper.")
+            return "tie"
+        } else if (computerChoice === "scissors") {
+            console.log("You lose! Scissors cut paper.")
+            return "computer"
+        } else {
+            throw new SyntaxError(`${computerChoice} is an invalid computerChoice!`)
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock crushes scissors.")
+            return "computer"
+        } else if (computerChoice === "paper") {
+            console.log("You win! Scissors cut paper.")
+            return "human"
+        } else if (computerChoice === "scissors") {
+            console.log("Tie! Both you and the computer chose scissors.")
+            return "tie"
+        } else {
+            throw new SyntaxError(`${computerChoice} is an invalid computerChoice!`)
+        }
+    } else {
+        throw new SyntaxError(`${humanChoice} is an invalid humanChoice!`)
+    }
+}
 
 // Increment the winner's (computer or human) score
 
